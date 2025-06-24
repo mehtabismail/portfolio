3a837e342981458a689bbc0c9e6edc1e50d026cf
Mehtab Ismail <mehtabismail@Mehtabs-MacBook-Pro-2.local>
2025-06-24 18:26:31 +0500

    picture and CV updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -17,11 +17,11 @@ export default function Intro() {
   return (
     <section
       ref={ref}
-      id="home"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      id='home'
+      className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'
     >
-      <div className="flex items-center justify-center">
-        <div className="relative">
+      <div className='flex items-center justify-center'>
+        <div className='relative'>
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -30,19 +30,28 @@ export default function Intro() {
               duration: 0.2,
             }}
           >
+            {/* <Image
+              src={require("@/public/Mehtab_Profile.jpg")}
+              alt='Mehtab portrait'
+              width='192'
+              height='192'
+              quality='100'
+              priority={true}
+              className='h-42 w-42 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
+            /> */}
             <Image
               src={require("@/public/Mehtab_Profile.jpg")}
-              alt="Mehtab portrait"
-              width="192"
-              height="192"
-              quality="95"
+              alt='Mehtab portrait'
+              width={192}
+              height={192}
+              quality={100}
               priority={true}
-              className="h-42 w-42 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className='w-48 h-48 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
             />
           </motion.div>
 
           <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
+            className='absolute bottom-0 right-0 text-4xl'
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
@@ -58,19 +67,19 @@ export default function Intro() {
       </div>
 
       <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Mehtab.</span> I'm a{" "}
-        <span className="font-bold">Senior React-Native developer</span> with{" "}
-        <span className="font-bold">5 years</span> of experience. I enjoy
-        building fully responsive <span className="italic">sites & apps</span>.
-        My focus is <span className="underline">React-Native & React.js</span>.
+        <span className='font-bold'>Hello, I'm Mehtab.</span> I'm a{" "}
+        <span className='font-bold'>Senior React-Native developer</span> with{" "}
+        <span className='font-bold'>5 years</span> of experience. I enjoy
+        building fully responsive <span className='italic'>sites & apps</span>.
+        My focus is <span className='underline'>React-Native & React.js</span>.
       </motion.h1>
 
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -78,38 +87,38 @@ export default function Intro() {
         }}
       >
         <Link
-          href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          href='#contact'
+          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
           }}
         >
           Contact me here{" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
+          href='/CV.pdf'
           download
         >
           Download CV{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/mehtabismail/"
-          target="_blank"
+          className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+          href='https://www.linkedin.com/in/mehtabismail/'
+          target='_blank'
         >
           <BsLinkedin />
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/mehtabismail"
-          target="_blank"
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+          href='https://github.com/mehtabismail'
+          target='_blank'
         >
           <FaGithubSquare />
         </a>
